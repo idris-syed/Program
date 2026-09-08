f132ce2bbd96f3be23bedcb58255637adb1a2593
idris-syed <syedidriszubair707@gmail.com>
2026-09-08 10:44:24 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,12 @@
+a=int(input('enter a value:'))
+b=int(input('enter b value:'))
+c=int(input('enter c value:'))
+
+if  (a>b) and (a>c):
+    print('a is largest')
+
+elif (b>c):
+    print('b is largest')
+
+else:
+    print('c is largest')
